fbbd334a7fef15c0f23d5181548f0f1623fef926
Aashutosh Poudel <aashutsohpoudyal@gmail.com>
2018-09-23 12:29:52 +0545

    update properties to load and initialize database

--- src/main/java/com/sankalpa/ictc_events/LoadDatabase.java
+++ src/main/java/com/sankalpa/ictc_events/LoadDatabase.java
@@ -32,32 +32,35 @@ public class LoadDatabase implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-//        // default admin
-//        adminRepository.save(new Admin("admin", "ict123"));
-////
-////
-////        // rooms
-//        roomRepository.save(new Room("Conference", 45, 0, "Conference", 10500, 1500, 0));
-//        roomRepository.save(new Room("Meeting Room", 45, 0, "Meeting", 5000, 1000, 0));
-//        roomRepository.save(new Room("Cafeteria", 30, 0, "Cafeteria", 5600, 800, 0));
+        // default admin
+        if (adminRepository.findAll().size() == 0)
+            adminRepository.save(new Admin("admin", "ict123"));
 //
-//        roomRepository.save(new Room("Lecture 1", 50, 1, "Lecture", 10500, 1500, 0));
-//        roomRepository.save(new Room("Lecture 2", 50, 1, "Lecture", 10500, 1500, 0));
-//        roomRepository.save(new Room("Lecture 3", 50, 1, "Lecture", 10500, 1500, 0));
-//        roomRepository.save(new Room("Lecture 4", 50, 1, "Lecture", 10500, 1500, 0));
 //
-//        roomRepository.save(new Room("Computer 1", 30, 2, "Computer", 18000, 0, 600));
-//        roomRepository.save(new Room("Computer 2", 30, 2, "Computer", 18000, 0, 600));
-//        roomRepository.save(new Room("Computer 3", 30, 2, "Computer", 18000, 0, 600));
-//        roomRepository.save(new Room("Computer 4", 30, 2, "Computer", 18000, 0, 600));
-//
-//        roomRepository.save(new Room("Computer 5", 30, 3, "Computer", 18000, 0, 600));
-//        roomRepository.save(new Room("Computer 6", 30, 3, "Computer", 18000, 0, 600));
-//        roomRepository.save(new Room("Language Lab 7", 30, 3, "Computer", 14000, 2000, 0));
-//        roomRepository.save(new Room("Computer 8", 30, 3, "Computer", 18000, 0, 600));
-//
-//        roomRepository.save(new Room("Seminar 1", 60, 4, "Seminar", 7500, 1200, 0));
-//        roomRepository.save(new Room("Seminar 2", 60, 4, "Seminar", 7500, 1200, 0));
+//        // rooms
+        if (roomRepository.findAll().size() == 0) {
+            roomRepository.save(new Room("Conference", 45, 0, "Conference", 10500, 1500, 0));
+            roomRepository.save(new Room("Meeting Room", 45, 0, "Meeting", 5000, 1000, 0));
+            roomRepository.save(new Room("Cafeteria", 30, 0, "Cafeteria", 5600, 800, 0));
+
+            roomRepository.save(new Room("Lecture 1", 50, 1, "Lecture", 10500, 1500, 0));
+            roomRepository.save(new Room("Lecture 2", 50, 1, "Lecture", 10500, 1500, 0));
+            roomRepository.save(new Room("Lecture 3", 50, 1, "Lecture", 10500, 1500, 0));
+            roomRepository.save(new Room("Lecture 4", 50, 1, "Lecture", 10500, 1500, 0));
+
+            roomRepository.save(new Room("Computer 1", 30, 2, "Computer", 18000, 0, 600));
+            roomRepository.save(new Room("Computer 2", 30, 2, "Computer", 18000, 0, 600));
+            roomRepository.save(new Room("Computer 3", 30, 2, "Computer", 18000, 0, 600));
+            roomRepository.save(new Room("Computer 4", 30, 2, "Computer", 18000, 0, 600));
+
+            roomRepository.save(new Room("Computer 5", 30, 3, "Computer", 18000, 0, 600));
+            roomRepository.save(new Room("Computer 6", 30, 3, "Computer", 18000, 0, 600));
+            roomRepository.save(new Room("Language Lab 7", 30, 3, "Computer", 14000, 2000, 0));
+            roomRepository.save(new Room("Computer 8", 30, 3, "Computer", 18000, 0, 600));
+
+            roomRepository.save(new Room("Seminar 1", 60, 4, "Seminar", 7500, 1200, 0));
+            roomRepository.save(new Room("Seminar 2", 60, 4, "Seminar", 7500, 1200, 0));
+        }
 
 //        // organizers
 //        organizerRepository.save(new Organizer("bhusal", "bhusal505", "Anish Bhusal",
